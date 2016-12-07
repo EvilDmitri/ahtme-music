fe4607e35f213cca76da28f00d7c38c7e68d64b2
EvilDmitri <dmitribrach@gmail.com>
2016-12-07 19:05:20 +0200

    templates

--- main.py
+++ main.py
@@ -3,12 +3,20 @@ import os
 import urllib
 
 from google.appengine.api import users
+from google.appengine.ext import ndb
 from google.appengine.ext import blobstore
 from google.appengine.ext import webapp
 from google.appengine.ext.webapp import blobstore_handlers
 from google.appengine.ext.webapp import template
 from google.appengine.ext.webapp.util import run_wsgi_app
 
+import jinja2
+
+# https://cloud.google.com/appengine/docs/python/getting-started/generating-dynamic-content-templates
+JINJA_ENVIRONMENT = jinja2.Environment(
+    loader=jinja2.FileSystemLoader(os.path.dirname(__file__)),
+    extensions=['jinja2.ext.autoescape'],
+    autoescape=True)
 
 class MainHandler(webapp.RequestHandler):
     def get(self):
